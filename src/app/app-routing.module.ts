1cacca693139093d9c4700d47f75792f7000b0b2
manigandan <manigandanm@hasnatech.onmicrosoft.com>
2022-11-18 18:08:26 +0530

    'update'

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AppComponent } from './app.component';
 import { LinkFrameComponent } from './link-frame/link-frame.component';
 import { NavbarComponent } from './navbar/navbar.component';
 
 const routes: Routes = [
 
-  {path:'link-frame',component:LinkFrameComponent},
+  // {
+  //   path:'link-frame',
+  //   component:AppComponent
+  // },
+  {
+    path:'link-frame',
+    component:LinkFrameComponent
+  },
 ];
 
 @NgModule({
